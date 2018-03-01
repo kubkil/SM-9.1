86d0d4f431f28f287c5a6c72d08374c59e01db38
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-01 20:34:50 +0100

    add getTriangleArea function

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function getTriangleArea (a, h) {
+  if (a < 0 || h < 0) {
+    return('Nieprawidłowe dane');
+  } else {
+    return(a*h/2);
+  }
+}
+console.log(getTriangleArea(10, 6));
+
+let getTriangleArea1 = getTriangleArea(-5, 1);
+let getTriangleArea2 = getTriangleArea(10, 3);
+let getTriangleArea3 = getTriangleArea(4, 6);
