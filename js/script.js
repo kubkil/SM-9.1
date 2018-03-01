0bce24f65acf5135a61f41e048ef6991cb7da682
kubkil <33201356+kubkil@users.noreply.github.com>
2018-03-01 22:56:14 +0100

    remove brackets in 'return'

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function getTriangleArea (a, h) {
   if (a < 0 || h < 0) {
-    return('Nieprawidłowe dane');
+    return 'Nieprawidłowe dane';
   } else {
-    return(a*h/2);
+    return a*h/2;
   }
 }
 console.log(getTriangleArea(10, 6));
